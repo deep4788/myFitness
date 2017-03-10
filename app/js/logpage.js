8c0951ace1d228a43fce3c9bb3c8329ef7568a52
Deep Aggarwal <deep.uiuc@gmail.com>
2017-03-09 22:14:32 -0600

    Added styling files, elements to log page Updated package.json

--- app/js/logpage.js
+++ app/js/logpage.js
@@ -1,3 +1,6 @@
 /*
  * This module handles log page functionality
  */
+
+//TODO create a confirmation dialog for when ADD button is clicked, the confirmation will show the added content for
+//an exercise
