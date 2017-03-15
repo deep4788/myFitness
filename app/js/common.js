606e775f3f52a0a2b882386773a1bb9d976b3dea
Deep Aggarwal <deep.uiuc@gmail.com>
2017-03-14 21:18:35 -0500

    Added thirdparty libraries: jquery and bootstrap Updated logpage.html with bootstrap

--- app/js/common.js
+++ app/js/common.js
@@ -5,6 +5,7 @@
 
 const { remote } = require('electron')
 
+//TODO change this to use jQuery
 document.querySelector(".home-page-button").addEventListener("click", function() {
     remote.getCurrentWindow().loadURL("file://" + __dirname + "/../index.html");
 });
--- app/js/startup.js
+++ app/js/startup.js
@@ -4,6 +4,7 @@
 
 const { remote } = require('electron')
 
+//TODO change these to use jQuery
 document.querySelector(".log-workout-button").addEventListener("click", function() {
     //remote.getCurrentWindow().loadURL('https://google.com')
     remote.getCurrentWindow().loadURL("file://" + __dirname + "/html/logpage.html");
